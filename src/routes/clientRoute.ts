3df4085269b85d12b5f300650df59a3639758e4f
Arif Sufri <arifsufri@Arifs-MacBook-Air.local>
2025-11-01 19:05:48 +0800

    a

--- src/routes/clientRoute.ts
+++ src/routes/clientRoute.ts
@@ -11,7 +11,6 @@ const router = express.Router();
 
 router.post('/register', registerClient);
 router.post('/login', loginClient);
-
 router.get('/', authenticateToken, getAllClients);
 router.get('/:id', authenticateToken, getClientById);
 
